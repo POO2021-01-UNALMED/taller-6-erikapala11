da41728654b8220b0fcd5c04e08eb9497f09c102
Erika Julieth Palacio Bedoya <75451391+erikapala11@users.noreply.github.com>
2021-04-19 17:42:47 -0500

    .

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -1,5 +1,47 @@
 package vehiculos;
 
-public class Automovil {
+class Automovil extends Vehiculo {
+
+	private int puestos;
+	private int puertas=4;
+	public int velocidadMaxima=100;
+	private String traccion="FWD";
+	
+	public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,
+			String traccion, Fabricante fabricante) {
+		super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
+	}
+
+	public int getPuestos() {
+		return puestos;
+	}
+
+	public void setPuestos(int puestos) {
+		this.puestos = puestos;
+	}
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
+	}
+
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
+
+	public String getTraccion() {
+		return traccion;
+	}
+
+	public void setTraccion(String traccion) {
+		this.traccion = traccion;
+	}
 
 }
